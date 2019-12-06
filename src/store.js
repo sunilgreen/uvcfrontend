82f009df1da5ad88b3557892901590a0a8b35eea
Sunil Green <green380@purdue.edu>
2019-12-06 13:09:21 -0500

    no reducer

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import rootReducer from "./reducers";
+//import rootReducer from "./reducers";
 
 
 const initialState = {};
@@ -9,7 +9,7 @@ const initialState = {};
 const middleware = [thunk];
 const store = createStore(
 //   () => [],
-  rootReducer,
+  //rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
